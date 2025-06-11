193c3cf965ad6bad7c3d9f8c0799c9a0314f9dd7
smiledelf <j.jason.k@hotmail.com>
2025-06-11 23:44:46 +1000

    modify backup-data.sh with new folder naming scheme

--- backup-data.sh
+++ backup-data.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# Get the current date in DD-MM-YYYY format
-current_date=$(date +"%d-%m-%Y")
+# Get the current date and time in DD-MM-YYYY_HH-MM-SS format
+current_datetime=$(date +"%Y.%m.%d %H.%M.%S")
 
 # Define source and destination directories
 source="minecraft-data"
-destination="minecraft-data-$current_date"
+destination="./minecraft-backups/minecraft-data $current_datetime"
 
 # Copy the directory recursively
 echo "Creating backup from $source ..."
