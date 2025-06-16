daf177f23d80f560bde7351c1cb3369b17a614ae
smiledelf <j.jason.k@hotmail.com>
2025-06-16 20:43:04 +1000

    fix echo not printing to standard output and add server messages to let players know backup is in progress

--- minecraft-backups/backup-data.sh
+++ minecraft-backups/backup-data.sh
@@ -5,20 +5,22 @@ source="../minecraft-data"
 destination="./minecraft-data $current_datetime"
 
 # Create backup: turn off saving, save all, copy the directory, turn on saving
-echo "$current_datetime Creating backup from $source ..." >> $logfile
-docker exec minecraft-mc-1 rcon-cli list >> $logfile
+echo "$current_datetime Creating backup from $source ..." | tee $logfile
+docker exec minecraft-mc-1 rcon-cli say Backing up world...
+docker exec minecraft-mc-1 rcon-cli list | tee $logfile
 docker exec minecraft-mc-1 rcon-cli save-off
 docker exec minecraft-mc-1 rcon-cli save-all
 cp -r "$source" "$destination"
 docker exec minecraft-mc-1 rcon-cli save-on
-echo "$current_datetime Created backup at: $destination" >> $logfile
+docker exec minecraft-mc-1 rcon-cli say Complete! Enjoy your day
+echo "$current_datetime Created backup at: $destination" | tee $logfile
 
 # Compressing
-echo "$current_datetime Compressing backup..." >> $logfile
+echo "$current_datetime Compressing backup..." | tee $logfile
 tar -cpzf "$destination.tar.gz" "$destination"
 rm -rf "$destination"
-echo "$current_datetime Compressed backup to $destination.tar.gz and removed the uncompressed backup." >> $logfile
+echo "$current_datetime Compressed backup to $destination.tar.gz and removed the uncompressed backup." | tee $logfile
 
 # Final log: add to log file
-echo "$current_datetime Finished backing up!" >> $logfile
-du -sh ./*
+echo "$current_datetime Finished backing up!" | tee $logfile
+du -sh ./* # <-- todo: highlight the new one?
