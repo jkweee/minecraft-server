126e3869555fe5919fece4d7efe94f0ffbec98aa
smiledelf <j.jason.k@hotmail.com>
2025-10-14 21:59:09 +1100

    remove nohup.out and add logging to status.py

--- status.py
+++ status.py
@@ -4,8 +4,10 @@ import json
 import os
 import re
 import time
+import logging
 from telert import send
 
+logger = logging.getLogger(__name__)
 
 """
 TODO: the point of this script is to let us know who is currently on, who has been on since I last checked, and whether the server has been backed up since the last player left
@@ -13,7 +15,16 @@ TODO: the point of this script is to let us know who is currently on, who has be
 - Approach 2: Read logs as it gets streamed
 """
 
-def execute_minecraft_command(command:str):
+def execute_minecraft_command(command:str) -> str:
+    """Execute a minecraft command via docker container
+
+    :param command: a string representing the command to send
+    :return: the command results from the server as a string
+    """
+
+    client = docker.from_env()
+    container = client.containers.get('minecraft-mc-1')
+
     exec_log = container.exec_run(f"rcon-cli {command}", stdout=True, stderr=True).output.decode()
 
     return exec_log
@@ -24,6 +35,7 @@ def get_server_population() -> dict:
 
     :return: a dictionary similar to the schema of status.json
     """
+    logger.info(f"Querying server to get server population using the 'list' command")
 
     list_players = str(execute_minecraft_command("list"))  # e.g. "There are 1 of a max of 10 players online: m1nefury"
 
@@ -38,43 +50,44 @@ def get_server_population() -> dict:
         "player_list": players
     }
 
+    logger.info(f"Finished querying server to get server population using the 'list' command and returned {server_population}")
+
     return server_population
 
 
 def get_last_server_population() -> dict:
 
-    # TODO: rewrite this, doesn't make sense to write something just to read it
-    # need to handle it not existing, esp. since git doesnt track
-    if not os.path.exists('status.json'):
-        default_status = {
+    logger.info(f"Reading the last known server population")
+
+    if os.path.exists('status.json'):
+        with open('status.json', 'r') as open_file:
+            last_status = json.load(open_file)
+    else:
+        last_status = {
+            "version": 1,
             "player_count": 0,
             "player_list": [],
             "last_checked": int(time.time()),
-            "backed_up": False
         }
 
-        with open('status.json', 'w') as f:
-            json.dump(default_status, f, indent=2)
+    logger.info(f"Finished reading the last known server population")
 
-    # read status.json to figure out the "last_state"
-    with open('status.json', 'r') as open_file:
-        last_status = json.load(open_file)
-
-    if last_status is not None:
-        last_status['player_count']
+    return last_status
 
 
-if __name__ == "__main__":
+def main():
+
+    # set up logging
+    logging.basicConfig(filename='server_status.log', level=logging.INFO)
+    console = logging.StreamHandler()
+    console.setLevel(logging.DEBUG)
+    logging.getLogger('').addHandler(console)
+    logger.info("Starting server status querying... :D")
 
     # change working directory to file path
     script_directory = os.path.dirname(os.path.abspath(__file__))
     os.chdir(script_directory)
-    print(f"Current working directory: {os.getcwd()}")
-
-    # get docker container
-    client = docker.from_env()
-    container = client.containers.get('minecraft-mc-1')
-
+    logger.info(f"Current working directory: {os.getcwd()}")
 
     """
     APPROACH 0
@@ -82,7 +95,6 @@ if __name__ == "__main__":
     If players are online, send a telert
     """
     # nohup python3 status.py &
-    # kill 123041 (pid)
 
     first_time = True
     last_player_count = 0
@@ -98,7 +110,7 @@ if __name__ == "__main__":
         difference_in_players = f"+{str(diff)}" if diff > 0 else str(diff)
 
         # construct status message
-        status_message = f"There are {current_player_count} players online (△{difference_in_players})"
+        status_message = f"There are {current_player_count} players online ({difference_in_players} △)"
         if current_player_count > 0:
             status_message += f": {current_players}"
         if last_player_count != current_player_count or first_time:
@@ -106,7 +118,7 @@ if __name__ == "__main__":
             if first_time:
                 first_time = False
 
-        print(f"{time.strftime('%Y-%m-%d %H:%M:%S')} - {status_message}")
+        logger.info(f"{time.strftime('%Y-%m-%d %H:%M:%S')} - {status_message}")
 
         last_player_count = current_player_count
 
@@ -137,4 +149,8 @@ if __name__ == "__main__":
     # results = execute_minecraft_command("list")
     # print(f"Sending this result: {results}")
     # send(results)
+
+
+if __name__ == "__main__":
+    main()
 
