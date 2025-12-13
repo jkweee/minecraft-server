4613ec435f8225ac78b0b7fc8369ee570829de3c
jkweee <j.jason.k@hotmail.com>
2025-12-13 22:55:18 +1100

    update server status monitor script. add a new ServerStatus class, new state file schema, and ability to keep the last login and logout timestamps of players. this will enable fun welcome messages.

--- server_status.py
+++ server_status.py
@@ -5,21 +5,119 @@ import os
 import re
 import time
 import logging
+from enum import Enum
 from telert import send
 
 logger = logging.getLogger(__name__)
 
-"""
-TODO: the point of this script is to let us know who is currently on, who has been on since I last checked, and whether the server has been backed up since the last player left
-- Approach 1: 5-minute timer (additional logic required to see if msg required)
-- Approach 2: Read logs as it gets streamed
+def now() -> int:
+    """Returns now as int in unix timestamp"""
+    return int(time.time())
 
-TODO: Needs a check to see if the container is even up, if the server is up, etc. 
-- Alert us if the server is down!
 
-"""
+class ServerStatus:
+    """A class to represent a server status. Can be read from or saved to a JSON file
 
-def execute_minecraft_command(command:str) -> str:
+    Some logic for handy reference:
+    - If login > logout: player is online
+    - If login < logout: player is offline
+    - "Last seen" is the last time the player appeared during a query
+    - Player list is a complete list of all players that have logged on.
+
+    """
+
+    def __init__(self, filepath:str=""):
+        """If a filepath is provided, read from status file automatically.
+
+        :param filepath: file to read from, defaults to "server_status.json"
+        """
+        if filepath != "":
+            self.read_from_file(filepath)
+        else:
+            self.version = 2 # schema version
+            self.server_last_queried = now()
+            self.player_details = {}
+
+
+    def read_from_file(self, filepath:str="server_status.json") -> None:
+        """Read previous status from file. Uses defaults if file doesn't exist.
+
+        :param filepath: file to read from, defaults to "server_status.json"
+        """
+        if os.path.exists(filepath):
+            with open(filepath, 'r') as open_file:
+                file = json.load(open_file)
+                self.version = file['version']
+                self.server_last_queried = file['server_last_queried']
+                self.player_details = file['player_details']
+
+
+    def save_to_file(self, filepath:str="server_status.json") -> None:
+        """Saves the server status to a file.
+
+        :param filepath: file to save to, defaults to "server_status.json"
+        """
+        server_population = {
+            "version": 2,
+            "server_last_queried": self.server_last_queried,
+            "player_details": self.player_details if len(self.player_details) > 0 else "null",
+        }
+
+        with open(filepath, 'w') as output:
+            json.dump(server_population, output, indent=4)
+
+
+    def get_online_players(self) -> list:
+        """Returns a list of players where is_online = True
+
+        :return: _description_
+        """
+        online_players = []
+        for player, details in self.player_details.items():
+            if details["is_online"] == True:
+                online_players.append(player)
+
+        return online_players
+
+
+    # boilerplate (sigh)
+    def set_version(self, version: int) -> None:
+        self.version = version
+
+    def get_version(self) -> int:
+        return self.version
+
+    def set_server_last_queried(self, timestamp: int) -> None:
+        self.server_last_queried = timestamp
+
+    def get_server_last_queried(self) -> int:
+        return self.server_last_queried
+
+    def set_player_details(self, player_details: dict) -> None:
+        self.player_details = player_details
+
+    def get_player_details(self) -> dict:
+        return self.player_details
+
+    def set_player_detail(self, player: str, detail: dict, value) -> None:
+        self.player_details[player][detail] = value
+
+    def get_player_detail(self, player: str, detail: dict) -> dict:
+        return self.player_details[player][detail]
+
+    def __str__(self) -> str:
+
+        status_as_string = str({
+            "version": 2,
+            "server_last_queried": self.server_last_queried,
+            "player_details": self.player_details
+        })
+
+        return status_as_string
+
+
+
+def query_server(command:str) -> str:
     """Execute a minecraft command via docker container
 
     :param command: a string representing the command to send
@@ -27,80 +125,120 @@ def execute_minecraft_command(command:str) -> str:
     """
 
     client = docker.from_env()
-    container = client.containers.get('minecraft-mc-1')
+    # container = client.containers.get('minecraft-mc-1')
+    container = client.containers.get('minecraft-dev-mc-1') # should this throw error???
 
     exec_log = container.exec_run(f"rcon-cli {command}", stdout=True, stderr=True).output.decode()
 
-    return exec_log
+    return str(exec_log)
 
 
-def get_server_population() -> dict:
-    """Query server for players and return a dict of the server population
+def query_online_players() -> list:
+    """Query server for players who are online
 
-    :return: a dictionary similar to the schema of status.json
+    :return: a list of online players
     """
-    logger.info(f"Querying server to get server population using the 'list' command")
-
-    list_players = str(execute_minecraft_command("list"))  # e.g. "There are 1 of a max of 10 players online: m1nefury"
-
-    num_match = re.search(r"There are (\d+)", list_players)
-    num_players = int(num_match.group(1)) if num_match else 0
+    
+    list_players = str(query_server("list"))  # e.g. "There are 1 of a max of 10 players online: m1nefury"
 
     players_match = re.search(r"online: (.+)", list_players)
     players = players_match.group(1).split(", ") if players_match else []
 
-    server_population = {
-        "version": 1,
-        "player_count": num_players,
-        "player_list": players,
-        "last_checked": int(time.time()),
-    }
-
-    logger.info(f"Finished querying server to get server population using the 'list' command and returned {server_population}")
-
-    return server_population
+    return players
 
 
-def get_previous_server_population() -> dict:
-    """Read previous server population from a json file
+def get_previous_server_status() -> ServerStatus:
+    """Gets the previous server status by reading from server_status.json
 
-    :return: dict representing the last known server population
+    :return: a ServerStatus object
+    """
+    return ServerStatus("server_status.json")
+
+
+def get_current_server_status() -> ServerStatus:
+
+    status = ServerStatus("server_status.json") # read from file as default
+
+    # update status on who is online and last seen
+    current_players = query_online_players()
+    status.set_server_last_queried(now())
+    for player in status.get_player_details().keys():
+        if player in current_players:
+            status.set_player_detail(player, "is_online", True)
+        else:
+            status.set_player_detail(player, "is_online", False)
+
+    return status
+
+
+def compare_population_difference(previous_status:ServerStatus, current_status:ServerStatus) -> tuple:
+    """Given the previous and current status, get a list of who logged on and who logged off in between
+    Returns a tuple of (logins, logout)
+
+    :param previous_status: _description_
+    :param current_status: _description_
+    :return: a tuple (list of players who logged in, list of players who logged out)
     """
 
-    logger.info(f"Reading the last known server population")
+    # we want: a list of logins, a list of logouts, based on the states
+    previously_online = set(previous_status.get_online_players())
+    currently_online = set(current_status.get_online_players())
 
-    if os.path.exists('server_status.json'):
-        with open('server_status.json', 'r') as open_file:
-            last_population = json.load(open_file)
-    else:
-        # increment version when making changes
-        last_population = {
-            "version": 1,
-            "player_count": 0,
-            "player_list": [],
-            "last_checked": int(time.time()),
-        }
+    logins = list(currently_online - previously_online)
+    logouts = list(previously_online - currently_online)
 
-    logger.info(f"Finished reading the last known server population")
-
-    return last_population
+    return (logins, logouts)
 
 
-def save_server_population_to_file(server_population:dict) -> None:
-    """Save server population to json file
+def update_login_and_logout_details(current_status:ServerStatus) -> ServerStatus:
+    # argument should be a ServerStatus representing the latest status
 
-    :param server_population: dict representing the last known server population
+    previous_status = get_previous_server_status()
+
+    new_logins, new_logouts = compare_population_difference(previous_status, current_status)
+    for player in new_logins:
+        current_status.set_player_detail(player, "last_login", now())
+    for player in new_logouts:
+        current_status.set_player_detail(player, "last_logout", now())
+
+    return current_status
+
+
+
+def send_telegram_updates() -> None:
+
     """
-    
-    logger.info(f"Saving server population data to json")
+        This function sends a Telegram update whenever there is a change in server population
+        Note:
+        - Uses logic built for the old status json
+        - This doesn't update the last_login or last_logout details
+    """
 
-    with open('server_status.json', 'w') as output:
-        json.dump(server_population, output, indent=4)
+    # read previous state and get current state
+    previous_status = get_previous_server_status()
+    previous_player_count = len(previous_status.get_online_players())
+    current_status = get_current_server_status()
+    current_players = current_status.get_online_players()
+    current_player_count = len(current_players)
 
-    logger.info(f"Finished saving server status to json file")
+    # construct status message
+    diff = current_player_count - previous_player_count
+    difference_in_players = f"+{str(diff)}" if diff > 0 else str(diff)
+
+    status_message = f"There are {current_player_count} players online ({difference_in_players} △)"
+    if current_player_count > 0:
+        status_message += f": {current_players}"
+    if previous_player_count != current_player_count:
+        send(status_message)
+
+    # save and log
+    current_status.save_to_file('server_status.json')
+    logger.info(f"{time.strftime('%Y-%m-%d %H:%M:%S')} - {status_message}")
 
 
-def main():
+
+
+if __name__ == "__main__":
 
     # change working directory to file path
     script_directory = os.path.dirname(os.path.abspath(__file__))
@@ -118,32 +256,4 @@ def main():
     console.setLevel(logging.DEBUG)
     logging.getLogger('').addHandler(console)
 
-    # get previous server population
-    previous_server_population = get_previous_server_population()
-    previous_player_count = previous_server_population["player_count"]
-
-    # get server population
-    server_population = get_server_population()
-    current_player_count = server_population["player_count"]
-    current_players = server_population["player_list"]
-
-    # format difference in players
-    diff = current_player_count - previous_player_count
-    difference_in_players = f"+{str(diff)}" if diff > 0 else str(diff)
-
-    # construct status message
-    status_message = f"There are {current_player_count} players online ({difference_in_players} △)"
-    if current_player_count > 0:
-        status_message += f": {current_players}"
-    if previous_player_count != current_player_count:
-        send(status_message)
-
-
-    logger.info(f"{time.strftime('%Y-%m-%d %H:%M:%S')} - {status_message}")
-
-    # save current server population to json
-    save_server_population_to_file(server_population)
-
-
-if __name__ == "__main__":
-    main()
+    send_telegram_updates()
